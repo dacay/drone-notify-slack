c5b31923087b4552facfbb5d018f2b0794db1dcc
Deniz Acay <denizacay@gmail.com>
2021-04-01 15:19:05 +0300

    Initial commit.

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:15-alpine
+
+WORKDIR /app
+
+COPY ["package.json", "package-lock.json*", "./"]
+
+RUN npm install --production
+
+COPY . .
+
+CMD [ "node", "index.js" ]
---
+++ index.js
@@ -0,0 +1,56 @@
+const { IncomingWebhook } = require('@slack/webhook');
+const giphyConstructor = require('giphy-api');
+
+const url = process.env.PLUGIN_WEBHOOK;
+const channel = process.env.PLUGIN_CHANNEL;
+const success_template = process.env.PLUGIN_SUCCESS_TEMPLATE;
+const error_template = process.env.PLUGIN_ERROR_TEMPLATE;
+const giphy_api_key = process.env.PLUGIN_GIPHY_API_KEY;
+
+if (!url) {
+    throw new Error("Missing webhook setting.");
+}
+
+if (!channel) {
+    throw new Error("Missing channel setting.");
+}
+
+if (!success_template) {
+    throw new Error("Missing success template setting.");
+}
+
+if (!error_template) {
+    throw new Error("Missing error template setting.");
+}
+
+if (!giphy_api_key) {
+    throw new Error("Missing Giphy API key setting.");
+}
+
+const giphy = giphyConstructor(giphy_api_key);
+
+const webhook = new IncomingWebhook(url);
+
+// Send the notification if build has a status
+if (process.env.DRONE_BUILD_STATUS) {
+
+    const buildStatus = process.env.DRONE_BUILD_STATUS;
+
+    giphy.random({
+        tag: buildStatus + " funny",
+        rating: 'pg',
+        fmt: 'json'
+    }, function (err, res) {
+        (async () => {
+          await webhook.send({
+            text: template,
+            attachments: [
+                {
+                    image_url: res.data.image_url          
+                }
+            ]
+          });
+        })();
+    });
+
+}
